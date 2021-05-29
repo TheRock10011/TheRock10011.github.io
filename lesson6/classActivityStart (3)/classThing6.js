920d258a74e33332e28802ecba66b58c660078a6
TheRock10011 <82978813+TheRock10011@users.noreply.github.com>
2021-05-28 18:47:35 -0600

    add

---
+++ lesson6/classActivityStart (3)/classThing6.js
@@ -0,0 +1,12 @@
+function Add() {
+         document.getElementById("name1");
+           thing = +document.getElementById("name1").value;
+           thing2 = +document.getElementById("name2").value;
+           thing3 = thing + thing2;
+           document.getElementById("final").innerText = thing3;
+           document.getElementById("final").style.backgroundColor="red";
+           document.getElementById("final").style.backgroundSize= 50;
+
+   }
+
+
